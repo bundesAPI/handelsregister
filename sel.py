7d223e0dfe623f31dc0240b19fe59fc4ec00fd49
Alper Çugun <github@alper.nl>
2022-08-20 14:24:54 +0200

    Replace Geckodriver with ChromeDriver for much better results

--- sel.py
+++ sel.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -8,21 +8,42 @@ import os
 import time
 
 # Need to set an environment variable to set headless window size
-os.environ['MOZ_HEADLESS_WIDTH'] = '1920'
-os.environ['MOZ_HEADLESS_HEIGHT'] = '1080'
+# os.environ['MOZ_HEADLESS_WIDTH'] = '1920'
+# os.environ['MOZ_HEADLESS_HEIGHT'] = '1080'
 
+# options = Options()
+# options.headless = True
+
+# options.set_preference("browser.download.folderList", 2)
+# options.set_preference("browser.download.manager.showWhenStarting", False)
+# options.set_preference("browser.download.panel.shown", False)
+# options.set_preference("browser.download.dir", os.getcwd())
+# options.set_preference("browser.helperApps.neverAsk.openFile", "application/pdf")
+# options.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/pdf")
+
+# driver = webdriver.Firefox(options=options)
+
+
+# Try Chrome
 options = Options()
+options.add_argument("--disable-extensions")
+options.add_argument("--window-size=1920,1080")
+options.add_experimental_option("prefs", {
+    "profile.default_content_settings.popups": 0,
+    "download.prompt_for_download": False,
+    "download.default_directory": os.getcwd(),
+    "download.directory_upgrade": True})
+
 options.headless = True
+# options.add_argument("--headless")
 
-options.set_preference("browser.download.folderList", 2)
-options.set_preference("browser.download.manager.showWhenStarting", False)
-options.set_preference("browser.download.panel.shown", False)
-options.set_preference("browser.download.dir", os.getcwd())
-options.set_preference("browser.helperApps.neverAsk.openFile", "application/pdf")
-options.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/pdf")
+driver = webdriver.Chrome(options=options)
 
-driver = webdriver.Firefox(options=options)
+
+
 driver.maximize_window()
+driver.set_window_size(1920, 1080)
+
 driver.get("https://www.handelsregister.de")
 
 
@@ -64,7 +85,7 @@ for doc_type in document_list:
     download_button = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//button[@id='form:kostenpflichtigabrufen']")))
     download_button.click()
 
-    time.sleep(10)
+    time.sleep(5)
 
     driver.back()
 
